3d81719ea37f5bdef42a3c7bfff847af154a10c7
Richard Cao <richardcao@Richards-Air.attlocal.net>
2022-02-20 11:45:48 -0800

    Added stats to graphs

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ class TradeHelper:
     
         self.queues = {
             symbol: {
+                'last_bid': None,
+                'last_ask': None,
                 'data': [],
                 'avg': [],
                 'times': []
@@ -35,16 +37,35 @@ class TradeHelper:
 
         # Plot
         self.fig, self.axs = plt.subplots(len(self.params['symbols']))
-        for i, symbol in enumerate(self.params['symbols']):
-            self.axs[i].yaxis.tick_right()
-            self.axs[i].title.set_text(symbol)
+        plt.subplots_adjust(
+                    left=0.05, 
+                    bottom=0.1,  
+                    right=0.7,  
+                    top=0.9,  
+                    wspace=0.4,  
+                    hspace=0.4)
+        self.fig.set_size_inches(10, 5)
         self.lines = {
             symbol: {
                 'data': self.axs[i].plot(self.queues[symbol]['times'], self.queues[symbol]['data'], c='black'),
                 'avg': self.axs[i].plot(self.queues[symbol]['times'], self.queues[symbol]['avg'], c='red')
             } for i, symbol in enumerate(self.params['symbols'])
         }
-        self.mock_vals = [np.random.normal(0,10) for _ in self.params['symbols']]
+        for i, symbol in enumerate(self.params['symbols']):
+            self.axs[i].yaxis.tick_right()
+            self.axs[i].yaxis.set_label_position("right")
+            self.axs[i].title.set_text(symbol)
+        self.mock_vals = [10 for _ in self.params['symbols']]
+
+    def get_stats(self, symbol):
+        window = self.queues[symbol]['data'][-self.window_size:]
+        if len(window) == 0:
+            return ""
+        return f"""
+                                    mean: {self.trunc(np.mean(window), 2)}, std: {self.trunc(np.std(window), 2)}
+                                    low: {self.trunc(np.min(window), 2)}, high: {self.trunc(np.max(window), 2)}
+                                    bid: {self.trunc(self.queues[symbol]['last_bid'], 2) or "-"}, ask: {self.trunc(self.queues[symbol]['last_ask'], 2) or "-"}
+        """
     
     def trunc(self, x, decimals):
         return int(x * 10**decimals) / 10**decimals
@@ -55,31 +76,20 @@ class TradeHelper:
         window = self.queues[symbol]['data'][-self.window_size:]
         now = datetime.now()
 
+        self.queues[symbol]['last_bid'] = msg['bid']
+        self.queues[symbol]['last_ask'] = msg['ask']
         self.queues[symbol]['data'].append(mid)
-        self.queues[symbol]['avg'].append(np.mean(window))
+        self.queues[symbol]['avg'].append(np.mean(window) if len(window) > 0 else mid)
         self.queues[symbol]['times'].append(now)
 
         if len(self.queues[symbol]['data']) > self.queue_capacity:
             self.queues[symbol]['data'].pop(0)
             self.queues[symbol]['avg'].pop(0)
             self.queues[symbol]['times'].pop(0)
-        
-    def log_stats(self, msg):
-        symbol = msg['symbol']
-        window = self.queues[symbol]['data'][-self.window_size:]
-
-        print("===")
-        print('mean:', self.trunc(np.mean(window), 2), ', std:', self.trunc(np.std(window), 2))
-        print('low:', self.trunc(np.min(window), 2), ', high:', self.trunc(np.max(window), 2))
-        print('bid:', self.trunc(msg['bid'], 2), ', ask:', self.trunc(msg['ask'], 2))
 
     def on_msg(self, msg):
         if msg['type'] == 'quote':
             self.update_queue(msg)
-            
-            # self.log_i = (self.log_i + 1) % self.log_interval
-            # if self.log_i == 0:
-            #     self.log_stats(msg)
 
     def run_ws(self):
         asyncio.run(tradier.ws_connect(self.params, on_msg=self.on_msg))
@@ -87,7 +97,7 @@ class TradeHelper:
     def mock_ws(self):
         for i in range(1000):
             for j in range(len(self.mock_vals)):
-                self.mock_vals[j] += np.random.normal(0, 1)
+                self.mock_vals[j] += np.random.normal(0, 0.1)
             msgs = [{
                 'type': 'quote',
                 'symbol': symbol,
@@ -104,12 +114,13 @@ class TradeHelper:
             self.axs[i].autoscale_view()
             self.lines[symbol]['data'][0].set_data(self.queues[symbol]['times'], self.queues[symbol]['data'])
             self.lines[symbol]['avg'][0].set_data(self.queues[symbol]['times'], self.queues[symbol]['avg'])
+            self.axs[i].set_ylabel(self.get_stats(symbol), rotation=0)
 
     def run(self):
         t = threading.Thread(target=self.run_ws if self.mode == 'live' else self.mock_ws)
         t.start()
 
-        ani = animation.FuncAnimation(self.fig, self.animate, interval=1000)
+        ani = animation.FuncAnimation(self.fig, self.animate, interval=3000)
         plt.show()
 
 def main(symbols, mode):
